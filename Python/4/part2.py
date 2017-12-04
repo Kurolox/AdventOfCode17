c37a58dc8a0bb971e7408e738079948200e03c62
Carlos Gómez <kurolox@gmail.com>
2017-12-04 19:31:02 +0100

    Fixed small leftover from previous try

--- Python/4/part2.py
+++ Python/4/part2.py
@@ -2,7 +2,6 @@ valid_inputs = 0
 
 with open("input", "r") as the_input:
     for line in the_input:
-        flag = True
         words = ["".join(sorted(word)) for word in line.split()]
         if len(words) == len(set(words)):
             valid_inputs += 1
