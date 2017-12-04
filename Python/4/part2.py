96f815cc9c3c3b325c22e40d18262f5a9c4d969a
Carlos Gómez <kurolox@gmail.com>
2017-12-04 19:28:22 +0100

    Day 4 part 2 solved in python

---
+++ Python/4/part2.py
@@ -0,0 +1,10 @@
+valid_inputs = 0
+
+with open("input", "r") as the_input:
+    for line in the_input:
+        flag = True
+        words = ["".join(sorted(word)) for word in line.split()]
+        if len(words) == len(set(words)):
+            valid_inputs += 1
+
+print(valid_inputs)
