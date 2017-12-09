7cb166529bf6b4e31072d0227f6db915d2df75cd
Carlos Gómez <kurolox@gmail.com>
2017-12-09 16:57:42 +0100

    Day 9 part 2 solved in python

---
+++ Python/9/part2.py
@@ -0,0 +1,29 @@
+with open("input", "r") as the_input:
+    full_input = "".join([line for line in the_input])
+
+index = 0
+garbage_mode = False
+value_counter = 0
+total_value = 0
+garbage_counter = 0
+
+while index < len(full_input):
+    if garbage_mode:
+        if full_input[index] == "!":
+            index += 1
+        elif full_input[index] == ">":
+            garbage_mode = False
+        else:
+            garbage_counter += 1 
+    else:
+        if full_input[index] == "<":
+            garbage_mode = True
+        elif full_input[index] == "{":
+            value_counter += 1
+        elif full_input[index] == "}":
+            total_value += value_counter
+            value_counter -= 1
+
+    index += 1
+
+print(garbage_counter)
